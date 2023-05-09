ad765c30da4efc86275c840e0130f4c4f2891b19
Jean-Christian Denis <contact@jcdenis.fr>
2023-05-09 10:27:06 +0200

    fix release 0.9.1

--- _define.php
+++ _define.php
@@ -18,7 +18,7 @@ $this->registerModule(
     'Uninstaller',
     'Uninstall cleanly plugins and themes',
     'Jean-Christian Denis and Contributors',
-    '0.9',
+    '0.9.1',
     [
         'requires'    => [['core', '2.26']],
         'permissions' => null,
